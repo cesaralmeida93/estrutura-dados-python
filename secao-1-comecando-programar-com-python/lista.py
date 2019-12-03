830cc518ed2c37439e9c134a028397d97f8f0ecc
cesar <cesar.almeida@znc.com.br>
2019-12-03 15:51:41 -0300

    adicionada manipulaçao de listas

---
+++ secao-1-comecando-programar-com-python/lista.py
@@ -0,0 +1,31 @@
+# manipulando listas
+
+# lista1 = [8, 17, 3, 6]
+# lista2 = [5, 6, 7, 8, 9, 10]
+
+# lista3 = lista1 + lista2
+# print(lista3)
+
+# lista1.pop(len(lista1)-1)
+# print(lista1)
+
+# lista2.remove(10)
+# print(lista2)
+
+# t_lista1 = tuple(lista1)
+# print(t_lista1)
+
+# lista1.append(10)
+# lista1.insert(1, 15) # insere na posicao 1 da lista o valor 15 
+# print(lista1)
+
+# lista1.sort()
+# print(lista1)
+
+# print(lista1[0]) # imprime a primeira posição
+
+# print(lista1[1:-1]) # imprime da posição 1 até a penultima posição(limite aberto)
+
+# print(lista1[1:]) # imprime da posição 1 até a ultima posição da lista
+
+# print(lista1[::-1]) # inverte a ordem da lista
